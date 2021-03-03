54f7953068510a25e7445e05e4825f20fbedde1c
Eric Lau <ericglau@outlook.com>
2021-03-02 21:52:39 -0500

    Initial commit

---
+++ install-vim-reach.sh
@@ -0,0 +1,8 @@
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/syntax/javascript.vim --create-dirs https://raw.githubusercontent.com/pangloss/vim-javascript/master/syntax/javascript.vim
+rm -rf vscode-reach
+unzip -d vscode-reach reachsh.reach-ide-0.12.0.vsix
+mkdir ~/.vim/reach-ide
+cp -R vscode-reach/extension/server ~/.vim/reach-ide
+rm -rf vscode-reach
+
